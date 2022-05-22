730176b4415662c7eee4d30bc76d5921e28f22be
Zsofia Horvath <horvathzsofia9105@gmail.com>
2022-05-22 20:43:32 +0100

    Update for running collectstatic in PRod

--- ucd_project_5/settings.py
+++ ucd_project_5/settings.py
@@ -14,7 +14,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')  # Configure path toward sta
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["serene-citadel-08400.herokuapp.com", "localhost"]
 

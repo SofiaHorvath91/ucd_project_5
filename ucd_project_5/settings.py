ea5d0716630cdc1a40452ce249aa7ba92b4959b2
Zsofia Horvath <horvathzsofia9105@gmail.com>
2022-05-22 21:30:49 +0100

    Update for running collectstatic in PRod

--- ucd_project_5/settings.py
+++ ucd_project_5/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
 
 # DATABASES['default'] = dj_database_url.config(default='postgres://txhaucrwmuxifl:db307114cf06f8980c201d9764826a195ba65de19ef1a77ff2d7f5fd762c968e@ec2-107-22-238-112.compute-1.amazonaws.com:5432/d6najr56rc9cok')
 
-db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
@@ -175,4 +175,4 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(config=locals())
+django_heroku.settings(config=locals(), staticfiles=False, logging=False)

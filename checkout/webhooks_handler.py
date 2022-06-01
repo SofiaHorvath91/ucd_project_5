98fc9c884eb5c9a537fa93fd3d657b96cb7f1cfe
Zsofia Horvath <horvathzsofia9105@gmail.com>
2022-06-01 23:59:15 +0100

    Update order line item webhook handler

--- checkout/webhooks_handler.py
+++ checkout/webhooks_handler.py
@@ -111,14 +111,15 @@ class StripeWH_Handler:
                     bag=bag,
                     stripe_pid=pid,
                 )
-                for item_id, quantity in json.loads(bag).items():
+                for item_id, item_data in json.loads(bag).items():
                     product = Book.objects.get(id=item_id)
-                    order_line_item = OrderLineItem(
-                        order=order,
-                        book=product,
-                        quantity=quantity,
-                    )
-                    order_line_item.save()
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            book=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
             except Exception as e:
                 if order:
                     order.delete()

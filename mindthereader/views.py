a6e9d9b333907be73658592ab3b9f439fe8928b9
Zsofia Horvath <horvathzsofia9105@gmail.com>
2022-06-05 01:10:05 +0100

    General styling, new background and home page completion

--- mindthereader/views.py
+++ mindthereader/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.contrib import messages
 from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.db.models import Q
@@ -12,7 +14,13 @@ User = get_user_model()
 
 # Home Page (home.html)
 def home(request):
+    context = {}
     books = Book.objects.all()
+
+    random_num = random.randint(0, (len(books) - 1))
+    random_book = books[random_num]
+    context['book'] = random_book
+
     if 'q' in request.GET:
         query = request.GET['q']
         if not query:
@@ -28,7 +36,7 @@ def home(request):
 
         return render(request, 'books/books.html', context)
 
-    return render(request, 'mindthereader/home.html')
+    return render(request, 'mindthereader/home.html', context)
 
 
 # Login Page (login.html)
@@ -47,10 +55,10 @@ def signin(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            context['successMsg'] = 'Successful login'
+            messages.success(request, 'Successful login')
             return render(request, 'mindthereader/login.html', context=context)
         else:
-            context['errorMsg'] = 'Invalid login'
+            messages.error(request, 'Invalid login')
             return render(request, 'mindthereader/login.html', context=context)
     else:
         return render(request, 'mindthereader/login.html')
@@ -75,23 +83,23 @@ def signup(request):
             # Check if username already exists
             try:
                 user1 = User.objects.get(username=username)
-                context['errorMsg'] = 'Username is already in system'
+                messages.error(request, 'Username is already in system')
                 return render(request, 'mindthereader/signup.html', context=context)
             except User.DoesNotExist:
                 # If username is unique, check if email already exists
                 try:
                     user2 = User.objects.get(email=email)
-                    context['errorMsg'] = 'Email is already in system'
+                    messages.error(request, 'Email is already in system')
                     return render(request, 'mindthereader/signup.html', context=context)
                 except User.DoesNotExist:
                     # If email and username are unique, create user
                     user = User.objects.create_user(username=username,
                                                     email=email,
                                                     password=password1)
-                    context['successMsg'] = "Thank you for signing up!"
+                    messages.success(request, "Thank you for signing up!")
                     return redirect('home')
         else:
-            context['errorMsg'] = 'Passwords must match'
+            messages.error(request, 'Passwords must match')
             return render(request, 'mindthereader/signup.html', context=context)
     else:
         return render(request, 'mindthereader/signup.html')
